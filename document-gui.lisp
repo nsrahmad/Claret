2a692d283da2f1ff81d67fe207fc537121f00b7a
Robert Strandh <robert.strandh@gmail.com>
2018-01-06 06:01:16 +0100

    Untabify for better tool support.

--- document-gui.lisp
+++ document-gui.lisp
@@ -5,14 +5,14 @@
 (defmethod display-component (pane (component document))
   (with-text-family (pane :sans-serif)
     (let ((boxes (loop for (child next) on (graph:children component)
-		       append (claret-boxes:make-boxes child pane nil)
-		       unless (null next)
+                       append (claret-boxes:make-boxes child pane nil)
+                       unless (null next)
                          append (list (claret-boxes:make-empty-box 1 30 0)))))
       (loop with x = 0
-	    with y = 0
-	    for box in boxes
-	    do (claret-gui:display-box box pane x (+ y (claret-boxes:ascent box)))
-	    do (incf y (+ (claret-boxes:ascent box) (claret-boxes:descent box)))))))
+            with y = 0
+            for box in boxes
+            do (claret-gui:display-box box pane x (+ y (claret-boxes:ascent box)))
+            do (incf y (+ (claret-boxes:ascent box) (claret-boxes:descent box)))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -20,8 +20,8 @@
 
 (define-command-table document-table
     :inherit-from (global-claret-table
-		   down-command-table
-		   forward-backward-command-table))
+                   down-command-table
+                   forward-backward-command-table))
 
 (defmethod find-claret-command-table (view (component document))
   (declare (ignore view))
