cbe7c232276fd01625927b225be4eff497dabd2b
Robert Strandh <robert.strandh@gmail.com>
2018-01-08 08:32:18 +0100

    Untabify for better tool support.

--- packages.lisp
+++ packages.lisp
@@ -1,69 +1,69 @@
 (defpackage #:graph
   (:use #:cl)
   (:export #:with-children-mixin
-	   #:children
-	   #:component
-	   #:timestamp
-	   #:number-of-children
-	   #:find-child
-	   #:at-beginning-p
-	   #:at-end-p
-	   #:add-child
-	   #:delete-child
-	   #:clone-component
-	   #:split-node
-	   #:merge-nodes
-	   #:with-cursor-set-mixin
-	   #:make-initial-cursor
-	   #:cursor-set
-	   #:cursor
-	   #:in-cursor-p
-	   #:root
-	   #:leaf
-	   #:location
-	   #:make-cursor
-	   #:clone-cursor
-	   #:move-up
-	   #:move-down
-	   #:move-forward
-	   #:move-backward
-	   #:move-to-beginning
-	   #:move-to-end))
+           #:children
+           #:component
+           #:timestamp
+           #:number-of-children
+           #:find-child
+           #:at-beginning-p
+           #:at-end-p
+           #:add-child
+           #:delete-child
+           #:clone-component
+           #:split-node
+           #:merge-nodes
+           #:with-cursor-set-mixin
+           #:make-initial-cursor
+           #:cursor-set
+           #:cursor
+           #:in-cursor-p
+           #:root
+           #:leaf
+           #:location
+           #:make-cursor
+           #:clone-cursor
+           #:move-up
+           #:move-down
+           #:move-forward
+           #:move-backward
+           #:move-to-beginning
+           #:move-to-end))
 
 (defpackage #:claret-document
   (:use #:cl #:graph #:esa-buffer)
   (:export #:document
-	   #:slots-to-be-saved
-	   #:read-document-from-stream
-	   #:write-document-to-stream))
+           #:slots-to-be-saved
+           #:read-document-from-stream
+           #:write-document-to-stream))
 
 (defpackage #:claret-boxes
     (:use #:clim-lisp)
   (:export #:box
-	   #:make-boxes
-	   #:vbox
-	   #:make-vbox
-	   #:hbox
-	   #:make-hbox
-	   #:make-empty-box
-	   #:ascent
-	   #:descent
-	   #:width
-	   #:children))
+           #:make-boxes
+           #:vbox
+           #:make-vbox
+           #:hbox
+           #:make-hbox
+           #:make-empty-box
+           #:ascent
+           #:descent
+           #:width
+           #:children))
 
 (defpackage #:claret-gui
   (:use #:clim-lisp #:clim #:esa #:esa-io #:esa-buffer #:claret-document)
   (:export #:display-frame-pane-vew
-	   #:display-box
-	   #:display-component
-	   #:current-view
-	   #:view-cursor
-	   #:find-claret-command-table
-	   #:global-claret-table
-	   #:up-command-table
-	   #:down-command-table
-	   #:forward-backward-command-table
-	   #:delete-child-command-table))
+           #:display-box
+           #:display-component
+           #:current-view
+           #:view-cursor
+           #:find-claret-command-table
+           #:global-claret-table
+           #:up-command-table
+           #:down-command-table
+           #:forward-backward-command-table
+           #:delete-child-command-table))
 
 (defpackage #:claret-document-gui
   (:use #:clim-lisp #:clim #:claret-document #:claret-gui :esa)
@@ -72,14 +72,14 @@
 (defpackage #:claret-section
   (:use #:clim-lisp #:clim #:claret-document #:claret-gui :esa)
   (:export #:section
-	   #:section-table))
+           #:section-table))
 
 (defpackage #:claret-paragraph
   (:use #:clim-lisp #:clim #:claret-document #:claret-gui :esa)
   (:export #:paragraph
-	   #:paragraph-table))
+           #:paragraph-table))
 
 (defpackage #:claret-word
   (:use #:clim-lisp #:clim #:claret-document #:claret-gui :esa)
   (:export #:word
-	   #:word-table))
+           #:word-table))
